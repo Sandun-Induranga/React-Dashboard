b58d5d18ed66cc2fc3496a58412d67b5b389079e
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-16 21:09:59 +0530

    Content Updated

--- src/components/slide-bar/slidebar.jsx
+++ src/components/slide-bar/slidebar.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import "./Slidebar.css"
+import React from "react";
+import "./Slidebar.css";
 
 function Slidebar() {
-
-    return (
-      <div className='w-25 h-100 bg-primary px-2'>
-        <h1>Slidebar</h1>
-        <button className='w-100 mb-5'>Dashboard</button>
-        <button className='w-100 mb-5'>Dashboard</button>
-        <button className='w-100 mb-5'>Dashboard</button>
-        <button className='w-100 mb-5'>Dashboard</button>
-        <button className='w-100 mb-5'>Dashboard</button>
-        <button className='w-100 mb-5'>Dashboard</button>
-      </div>
-    );
+  return (
+    <div className="w-25 h-100 bg-white px-2">
+      <h1>Slidebar</h1>
+      <button className="w-100 mb-5 d-flex px-5"><p><i class="bi bi-grid-1x2-fill"></i></p>Dashboard</button>
+      <button className="w-100 mb-5 d-flex px-5"><p><i class="bi bi-currency-exchange"></i></p>Exchange</button>
+      <button className="w-100 mb-5 d-flex px-5"><p><i class="bi bi-book"></i></p>Library</button>
+      <button className="w-100 mb-5 d-flex px-5"><p><i class="bi bi-calendar2-check"></i></p>Schedules</button>
+      <button className="w-100 mb-5 d-flex px-5"><p><i class="bi bi-wallet"></i></p>Payouts</button>
+      <button className="w-100 mb-5 d-flex px-5"><p><i class="bi bi-gear"></i></p>Settings</button>
+    </div>
+  );
 }
 
 export default Slidebar;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Slidebar from './components/slide-bar/slidebar';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
