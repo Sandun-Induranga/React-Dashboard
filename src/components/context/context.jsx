09cdd2e4b6053792eda93bbe211f8f03e64f868e
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-17 18:53:51 +0530

    Content Updated

--- src/components/context/context.jsx
+++ src/components/context/context.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Context.css";
+import Card from "../card/card";
 
-function Context(){
-    return (
-        <div className="context">
-
-        </div>
-    );
+function Context() {
+  return (
+    <div className="context">
+      <section className="d-flex justify-content-around mt-2 mb-3">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </section>
+    </div>
+  );
 }
 
 export default Context;
