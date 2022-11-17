1c85e48c378cc799fb5424408b5dfaef27932a21
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-17 18:37:44 +0530

    Content Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Slidebar from "./components/slide-bar/slidebar";
 import Topbar from "./components/top-bar/topbar";
+import Card from "./components/card/card"; 
 
 function App() {
   return (
     <div className="App">
       <Slidebar />
       <Topbar />
+      <Card />
     </div>
   );
 }
---
+++ src/components/card/card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Card.css";
+
+function Card(){
+    return (
+        <div>
+            card
+        </div>
+    );
+}
+
+export default Card;
