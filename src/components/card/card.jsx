95861ed925e6bcc6a10bc6432fc75946fae5e100
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-17 19:59:50 +0530

    Content Updated

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./Card.css";
 
-function Card(props){
-    return (
-        <div className="card rounded-4 border-0">
-            <section className="border-0">
-                <p>{props.name}</p>
-                <p>{props.price}</p>
-                <p>{props.rate}</p>
-            </section>
+function Card(props) {
+  return (
+    <div className="card rounded-4 border-0 d-flex">
+      <section className="border-0 d-flex justify content-start flex-column">
+        <div className="d-flex bg-success justify-content-center align-items-center icon">
+          <h2>
+            <i class="bi bi-currency-bitcoin"></i>
+          </h2>
         </div>
-    );
+        <p>{props.name}</p>
+        <p>{props.price}</p>
+        <p>{props.rate}</p>
+      </section>
+      <section>
+        <img src="" alt="image" srcset="" />
+      </section>
+    </div>
+  );
 }
 
 export default Card;
