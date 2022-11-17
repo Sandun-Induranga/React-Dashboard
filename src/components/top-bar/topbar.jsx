2a7591263e1ac5cbc678c3669c85b44405a6efee
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-17 10:16:24 +0530

    Content Updated

--- src/components/top-bar/topbar.jsx
+++ src/components/top-bar/topbar.jsx
@@ -3,8 +3,16 @@ import "./Topbar.css";
 
 function Topbar() {
     return(
-        <div className="top-bar">
-            TopBar
+        <div className="top-bar d-flex justify-content-between align-items-center">
+            <section>
+                Hello John Devid
+            </section>
+            <section>
+                <input type="text" />
+            </section>
+            <section>
+                Notification
+            </section>
         </div>
     );
 }
