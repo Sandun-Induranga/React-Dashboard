fc79cabf2886f0d7e659b0518416b8807199e02e
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-21 12:58:58 +0530

    Content Updated

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Card.css";
 
+
 function Card(props) {
   return (
     <div className="card rounded-4 border-0 d-flex flex-row justify-content-around align-items-center">
       <section className="border-0 d-flex justify content-start flex-column card-content">
         <div className="d-flex bg-success justify-content-center align-items-center icon mb-2">
           <h2>
-            {getCoin(props.icon)}
+            {getCoin(props.name)}
           </h2>
         </div>
         <p className="m-0">{props.name}</p>
@@ -21,8 +22,11 @@ function Card(props) {
 
 function getCoin(value) {
   switch (value) {
-    case "bitcoin":
-      return <i className="bi bi-currency-bitcoin m-0"></i>
+    case "BTC":
+      return <i className="bi bi-currency-bitcoin m-0"></i>;
+
+      case "LTC":
+      return <i className="fa-solid fa-litecoin-sign"></i>;
   
     default:
       // return <i className="bi bi-currency-bitcoin m-0"></i>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
