780fac2d6e067c931bd5096cd0b0944c9cc23c5d
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-21 13:56:24 +0530

    Content Updated

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./Card.css";
-
+import btc from "../../Coins-1.png";
+import ltc from "../../Coins-2.png";
+import etm from "../../Coins-3.png";
+// import bnb from "../../Coins-4.png";
 
 function Card(props) {
   return (
     <div className="card rounded-4 border-0 d-flex flex-row justify-content-around align-items-center">
       <section className="border-0 d-flex justify content-start flex-column card-content">
         <div className="d-flex bg-success justify-content-center align-items-center icon mb-2">
-          <h2>
-            {getCoin(props.name)}
-          </h2>
+          {/* <h2> */}
+          {/* </h2> */}
+          {getCoin(props.name)}
         </div>
         <p className="m-0">{props.name}</p>
         <p className="m-0">{props.price}</p>
@@ -23,13 +26,17 @@ function Card(props) {
 function getCoin(value) {
   switch (value) {
     case "BTC":
-      return <i className="bi bi-currency-bitcoin m-0"></i>;
+      return <img src={btc} alt="BTC" />;
 
-      case "LTC":
-      return <i className="fa-solid fa-house"></i>;
-  
+    case "LTC":
+      return <img src={ltc} alt="BTC" />;
+
+    case "LTC":
+      return <img src={ltc} alt="BTC" />;
+
     default:
       // return <i className="bi bi-currency-bitcoin m-0"></i>
+      return <img src={btc} alt="BTC" />;
       break;
   }
 }
