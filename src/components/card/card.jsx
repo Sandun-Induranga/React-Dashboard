d22648481ea7d505f3dbf0c0d09d5c9385e71196
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-21 11:50:24 +0530

    Content Updated

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -5,18 +5,16 @@ function Card(props) {
   return (
     <div className="card rounded-4 border-0 d-flex flex-row justify-content-around align-items-center">
       <section className="border-0 d-flex justify content-start flex-column card-content">
-        <div className="d-flex bg-success justify-content-center align-items-center icon">
+        <div className="d-flex bg-success justify-content-center align-items-center icon mb-2">
           <h2>
             <i class="bi bi-currency-bitcoin m-0"></i>
           </h2>
         </div>
-        <p>{props.name}</p>
-        <p>{props.price}</p>
-        <p>{props.rate}</p>
+        <p className="m-0">{props.name}</p>
+        <p className="m-0">{props.price}</p>
+        <p className="m-0">{props.rate}</p>
       </section>
-      <section className="border-0 graph">
-        graph
-      </section>
+      <section className="border-0 graph"></section>
     </div>
   );
 }
