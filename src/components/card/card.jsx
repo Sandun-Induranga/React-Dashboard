7641e337355526bf6d471fb9db54c2e05af27e9b
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-26 21:27:26 +0530

    Content Updated

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -9,9 +9,7 @@ function Card(props) {
   return (
     <div className="card rounded-4 border-0 d-flex flex-row justify-content-around align-items-center">
       <section className="border-0 d-flex justify content-start flex-column card-content">
-        <div className="d-flex bg-success justify-content-center align-items-center icon mb-2">
-          {/* <h2> */}
-          {/* </h2> */}
+        <div className="d-flex justify-content-center align-items-center icon mb-2">
           {getCoin(props.name)}
         </div>
         <p className="m-0">{props.name}</p>
