7242ea083c089133301099831842d9d07c382120
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-27 10:50:55 +0530

    Content Updated

--- src/components/graph/graph.jsx
+++ src/components/graph/graph.jsx
@@ -5,9 +5,16 @@ import graph from "../../assets/Graph.png";
 function Graph(props) {
     return (
         <div className="chart bg-white d-flex flex-column justify-content-between">
-            <section>
+            <section className="d-flex justify-content-between">
                 <aside>
                     <h4>Market Overview</h4>
+                </aside>
+                <aside>
+                    <div>ALL</div>
+                    <div>1M</div>
+                    <div>6M</div>
+                    <div>1Y</div>
+                    <div>YTD</div>
                 </aside>
             </section>
             <img src={graph} alt=""/>
