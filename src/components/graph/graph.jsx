aa598026c2f8e12fffc5266e972e76fa97b94f10
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-27 11:06:28 +0530

    Content Updated

--- src/components/graph/graph.jsx
+++ src/components/graph/graph.jsx
@@ -6,8 +6,9 @@ function Graph(props) {
     return (
         <div className="chart bg-white d-flex flex-column justify-content-between">
             <section className="d-flex justify-content-between m-2">
-                <aside>
+                <aside className="d-flex flex-column">
                     <h4>Market Overview</h4>
+                    <p>7.2141,997.5(25%)</p>
                 </aside>
                 <aside className="d-flex w-50 justify-content-around options">
                     <div className="d-flex justify-content-center align-items-center rounded-4">ALL</div>
