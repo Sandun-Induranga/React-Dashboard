64484038b518e0ca25a8aea737f6f97faa90db52
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-27 11:03:39 +0530

    Content Updated

--- src/components/graph/graph.jsx
+++ src/components/graph/graph.jsx
@@ -5,16 +5,16 @@ import graph from "../../assets/Graph.png";
 function Graph(props) {
     return (
         <div className="chart bg-white d-flex flex-column justify-content-between">
-            <section className="d-flex justify-content-between">
+            <section className="d-flex justify-content-between m-2">
                 <aside>
                     <h4>Market Overview</h4>
                 </aside>
                 <aside className="d-flex w-50 justify-content-around options">
-                    <div>ALL</div>
-                    <div>1M</div>
-                    <div>6M</div>
-                    <div>1Y</div>
-                    <div>YTD</div>
+                    <div className="d-flex justify-content-center align-items-center rounded-4">ALL</div>
+                    <div className="d-flex justify-content-center align-items-center rounded-4">1M</div>
+                    <div className="d-flex justify-content-center align-items-center rounded-4">6M</div>
+                    <div className="d-flex justify-content-center align-items-center rounded-4">1Y</div>
+                    <div className="d-flex justify-content-center align-items-center rounded-4">YTD</div>
                 </aside>
             </section>
             <img src={graph} alt=""/>
