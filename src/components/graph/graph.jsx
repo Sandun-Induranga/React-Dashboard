5f0f3041b894dbb751e1b317db39b477737ca92a
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-27 10:39:38 +0530

    Content Updated

--- src/components/graph/graph.jsx
+++ src/components/graph/graph.jsx
@@ -4,8 +4,11 @@ import graph from "../../assets/Graph.png";
 
 function Graph(props) {
     return (
-        <div className="chart bg-white">
-            <img src={graph} alt="" srcset="" />
+        <div className="chart bg-white d-flex flex-column justify-content-between">
+            <section>
+                Header
+            </section>
+            <img src={graph} alt=""/>
         </div>
     );
 }
