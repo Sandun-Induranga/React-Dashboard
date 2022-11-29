358e915585e98f15b3831ad9e38500e6cd3657ac
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-29 13:34:16 +0530

    Content Updated

--- src/components/balances/balances.jsx
+++ src/components/balances/balances.jsx
@@ -4,7 +4,7 @@ import "./balances.css";
 function Balances(){
     return(
         <div className="balances">
-            <section>
+            <section className="m-2 d-flex justify-content-around">
                 <h4>Balances</h4>
                 <i class="bi bi-plus-square-fill"></i>
             </section>
