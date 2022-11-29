0919a229514f6af6110d9199d2690cdbe416df0f
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-29 16:54:37 +0530

    Content Updated

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./team.css";
+import admin from "../../assets/admin.png";
+import user from "../../assets/user.png";
 
 function Team() {
   return (
@@ -7,9 +9,22 @@ function Team() {
       <h1 className="m-2">Team</h1>
 
       <aside className="d-flex flex-column align-items-center">
-        <section className="m-2"></section>
+        <section className="m-2 d-flex">
+            <img src={admin} alt="" />
+            <section>
+                <h5 className="mt-2">Total Admins</h5>
+                <p>6</p>
+            </section>
+        </section>
 
-        <section className="m-2"></section>
+        <section className="m-2 d-flex">
+            <img src={user} alt="" />
+            <section>
+                <h5 className="mt-2">Total Users</h5>
+                <p>10</p>
+            </section>
+        </section>
+
       </aside>
     </div>
   );
