03eba829551b0706d5fe487877ffb769f1147d05
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-29 16:48:47 +0530

    Content Updated

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./team.css";
 
-function Team(){
-    return(
-        <div className="team bg-white m-2">
-            <h1 className="m-2">Team</h1>
-        </div>
-    );
+function Team() {
+  return (
+    <div className="team bg-white m-2">
+      <h1 className="m-2">Team</h1>
+
+      <aside className="d-flex flex-column align-items-center">
+        <section className="m-2"></section>
+
+        <section className="m-2"></section>
+      </aside>
+    </div>
+  );
 }
 
 export default Team;
