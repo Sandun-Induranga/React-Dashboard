dd687e0aea6d3149fcc247b0b791610072ee7c0f
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-29 12:42:55 +0530

    Content Updated

--- src/components/balances/balances.jsx
+++ src/components/balances/balances.jsx
@@ -4,7 +4,10 @@ import "./balances.css";
 function Balances(){
     return(
         <div className="balances">
-
+            <section>
+                <h4>Balances</h4>
+                <i class="bi bi-plus-square-fill"></i>
+            </section>
         </div>
     )
 }
